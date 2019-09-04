f996ab3f357e7a9601a300f26795656bbafb93ce
Ana <dia151378@uvg.edu.gt>
2019-09-03 20:34:04 -0600

    empezando a ver rondas

--- DiagramadePresedencia.py
+++ DiagramadePresedencia.py
@@ -10,4 +10,20 @@ df = pd.DataFrame({'Start Time': pd.to_datetime(start_times),
               'End Time' : pd.to_datetime(end_times)}, index=index)
 print(df)
 
-
+#cubo de tiempo 
+rng = pd.date_range('2019-08-08 20:00', periods=1, freq='H')
+ts = pd.DataFrame(0, index=rng, columns=['minutes'], dtype='float')
+print(ts)
+print(rng)
+rng = pd.date_range('2019-08-08 20:00', periods=1, freq='H')
+ts = pd.DataFrame(0, index=rng, columns=['minutes'], dtype='float')
+print(ts)
+print(rng)
+rng = pd.date_range('2019-08-10 20:00', periods=1, freq='H')
+ts = pd.DataFrame(0, index=rng, columns=['minutes'], dtype='float')
+print(ts)
+print(rng)
+rng = pd.date_range('2019-08-12 20:00', periods=2, freq='H')
+ts = pd.DataFrame(0, index=rng, columns=['minutes'], dtype='float')
+print(ts)
+print(rng)
